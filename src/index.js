3ea953f90bcda3dde5687ff3782fd8fed1ec0113
Binod Chaudhary <thebinod7@gmail.com>
2021-07-26 09:08:41 +0545

    set truffle configs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './assets/css/custom.css';
+import App from './modules/app';
 
 ReactDOM.render(
 	<React.StrictMode>
